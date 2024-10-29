7e5d3c7a71bee2aba1ce4fa0501fa7a07b46545c
iqbalart434 <iqbalart434@gmail.com>
2024-10-29 13:47:35 +0000

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<!-- Suggested code may be subject to a license. Learn more: ~LicenseLog:1263070994. -->
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    
+<!-- Suggested code may be subject to a license. Learn more: ~LicenseLog:1948338282. -->
+<head>
+    <a href="/">Home</a>
+    <a href="/about">about</a>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hamalan about</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<h1>Halaman About iqbal</h1>
+<h3>owner: {{ $nama }}</h3>
+<img src="img/iqbal.JPG" alt="iqbal" width="200" height="200">
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Home</title>
+    <link rel="stylesheet" href="/css/style.css">
+<style>
+    body{
+    background-color:lightblue;
+}
+</style>
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">about</a>
+    <h1>Halaman Home</h1>
+
+    
+    <script src="js/script.js"></script>
+</body>
+</html>
